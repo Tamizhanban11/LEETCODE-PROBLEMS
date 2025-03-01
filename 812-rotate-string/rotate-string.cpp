9d46faa39e304aec4c084d65d1e8d3cd834bca10
TAMIZHANBAN N <145478005+Tamizhanban11@users.noreply.github.com>
2025-03-01 11:01:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (61.72%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-    if(s.length()!= goal.length()){
+        if(s.length()!=goal.length())
         return false;
-    }
-    string newstring = s+s;
-    return newstring.contains(goal);
+        string newstring = s+s;
+        return newstring.contains(goal);
         
     }
 };
