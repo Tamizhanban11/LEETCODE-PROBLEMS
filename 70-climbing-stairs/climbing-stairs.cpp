5458ea8c526e8642bc43ce69f8cb9d9d429fd60a
TAMIZHANBAN N <145478005+Tamizhanban11@users.noreply.github.com>
2025-03-17 13:58:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (71.47%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -5,7 +5,7 @@ public:
         int second = 1;
         for(int i = n-1;i>0;i--){
             int temp = first;
-            first= first + second;
+            first = first+second;
             second = temp;
         }
         return first;
