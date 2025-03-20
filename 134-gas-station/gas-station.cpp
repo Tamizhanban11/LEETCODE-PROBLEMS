6f8f3e1e15d01e487db1df2281c8149f96249471
TAMIZHANBAN N <145478005+Tamizhanban11@users.noreply.github.com>
2025-03-20 11:41:09 +0530

    Time: 0 ms (100.00%) | Memory: 112.5 MB (42.51%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -6,27 +6,22 @@ public:
         for(int i = 0;i<n;i++){
             totalLeft += gas[i]-cost[i];
         }
-            if(totalLeft<0){
-                return -1;
-            }
-        
-            int StartIndex = 0;
-            int Remaining= 0;
-            for(int i = 0;i<n;i++){
-                int currentGas = gas[i]+ Remaining;
-                int currentCost = cost[i];
-                Remaining = currentGas - currentCost;
-    
-    if(Remaining<0){
-        Remaining = 0;
-        StartIndex = i+1;
-    }
+        if(totalLeft < 0) return -1;
 
-            }
-
+        int StartIndex = 0;
+        int Remaining = 0;
+        for(int i = 0;i<n;i++){
+            int CurrentGas=gas[i]+Remaining;
+            int currentCost = cost[i];
+            Remaining = CurrentGas-currentCost;
         
+        if(Remaining <0){
+                Remaining = 0;
+                StartIndex = i+1;
+        }
+        }
         return StartIndex;
-    }
+
         
-    
+    }
 };
