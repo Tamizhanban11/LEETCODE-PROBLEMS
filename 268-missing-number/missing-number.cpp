f043397220cd3b8d556e00422132e4403bcf4152
TAMIZHANBAN N <145478005+Tamizhanban11@users.noreply.github.com>
2025-04-17 20:19:28 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (92.43%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         int sum = n*(n+1)/2;
         int sum2 = 0;
-        for(int i = 0;i<n;i++){
+        for(int i =0;i<n;i++){
             sum2+=nums[i];
         }
         return (sum - sum2);
