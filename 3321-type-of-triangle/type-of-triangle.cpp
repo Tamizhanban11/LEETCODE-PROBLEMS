691c8cffa77bceadfed4c86691e785f81dafb819
TAMIZHANBAN N <145478005+Tamizhanban11@users.noreply.github.com>
2025-05-28 18:40:22 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (67.95%) - LeetSync

---
+++ 3321-type-of-triangle/type-of-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        if (nums[0] + nums[1] <= nums[2]) {
+            return "none";
+        }
+        if (nums[0] == 0 || nums[1] == 0 || nums[2] == 0) {
+            return "none";
+        }
+        if (nums[0] == nums[1] && nums[1] == nums[2]) {
+            return "equilateral";
+        }
+        if (nums[0] != nums[1] && nums[1] != nums[2] && nums[0] != nums[2]) {
+            return "scalene";
+        }
+        return "isosceles";
+    }
+};
